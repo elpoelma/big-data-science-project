d8f4b7c0566d2445ab7dc0d9b0523260274b4594
Jan-Pieter Baert <jan-pieter@baert.jp.net>
2021-03-18 19:55:48 +0100

    Add data loading function

--- main.py
+++ main.py
@@ -4,14 +4,23 @@ findspark.init()
 import pyspark
 from pyspark.sql.functions import avg
 
-fileLimit = 2
-path = "./data"
 
-conf = pyspark.SparkConf().setMaster("local[2]").setAppName("loading").set('spark.jars', './spark-scifio/target/scifio-spark-datasource-fat.jar')
-sc = pyspark.SparkContext(conf=conf)
-spark = pyspark.sql.SparkSession(sc)
+def load_data():
+    fileLimit = 2
+    path = "./data"
 
-df = spark.read.format("scifio").option("path", path).option("filelimit", fileLimit).option("numpartitionsperfile", 5).option("channels", "1").option("masked", True).load().cache()
+    conf = pyspark.SparkConf().setMaster("local[2]").setAppName("loading").set('spark.jars', './spark-scifio/target/scifio-spark-datasource-fat.jar')
+    sc = pyspark.SparkContext(conf=conf)
+    spark = pyspark.sql.SparkSession(sc)
 
-print("OUTPUT BELOW:")
-df.agg(avg(df["width"])).show()
+    channels = "1,2,3,4,5,6,7,8,9"
+    parti = 5
+    df = spark.read.format("scifio").option("path", path).option("filelimit", fileLimit).option("numpartitionsperfile", parti).option("channels", channels).option("masked", True).load().cache()
+
+    print("OUTPUT BELOW:")
+    # df.show()
+    # print(df.count())
+    return df
+
+
+print(load_data().count())
